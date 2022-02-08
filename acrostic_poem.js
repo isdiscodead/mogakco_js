e01073890729a9ed320730f6a59c36368ef12d83
isdiscodead <isdiscodead@korea.ac.kr>
2022-02-08 18:23:56 +0900

    mogakco day03 basic

---
+++ acrostic_poem.js
@@ -0,0 +1,14 @@
+function btn_1() {
+    mo = document.getElementsByTagName("span")[0];
+    mo.innerHTML = "처럼이니";
+}
+
+function btn_2() {
+    gak = document.getElementsByTagName("span")[1];
+    gak.innerHTML = "잡고 열심히 하면";
+}
+
+function btn_3() {
+    co = document.getElementsByTagName("span")[2];
+    co.innerHTML = "린이 탈출이다 😎";
+}
