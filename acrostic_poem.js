879531bd9a70ca582ff03f1c9a2660c026596ad0
isdiscodead <isdiscodead@korea.ac.kr>
2022-02-08 18:31:16 +0900

    mogakco day04 upgrade ver

--- acrostic_poem.js
+++ acrostic_poem.js
@@ -1,14 +1,14 @@
-function btn_1() {
-    mo = document.getElementsByTagName("span")[0];
-    mo.innerHTML = "처럼이니";
-}
+function btn_onclick(id) {
+    if ( id == "mo" ) {
+        btn = document.getElementsByTagName("span")[0];
+        btn.innerHTML = "처럼이니";
 
-function btn_2() {
-    gak = document.getElementsByTagName("span")[1];
-    gak.innerHTML = "잡고 열심히 하면";
-}
+    } else if ( id == "gak" ) {
+        btn = document.getElementsByTagName("span")[1];
+        btn.innerHTML = "잡고 열심히 하면";
 
-function btn_3() {
-    co = document.getElementsByTagName("span")[2];
-    co.innerHTML = "린이 탈출이다 😎";
+    } else {
+        btn = document.getElementsByTagName("span")[2];
+        btn.innerHTML = "린이 탈출이다 😎";
+    }
 }
