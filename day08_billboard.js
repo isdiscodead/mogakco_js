3ecbc097fd23a7fd27e6ebb52ce4974f07d887cb
isdiscodead <isdiscodead@korea.ac.kr>
2022-02-16 23:47:29 +0900

    day 08

--- day08_billboard.js
+++ day08_billboard.js
@@ -2,15 +2,15 @@ var h2 = document.getElementsByTagName('h2')[0];
 var txt =  'HELLO WORLD'
 var i = 0;
 
-function btn_display(){
-    if(i < txt.length){
-        h1[0].innerHTML +=txt[i];
+function display(){
+    if( i < txt.length ){
+        h2.innerHTML += txt[i];
         i++
     }
-    colorChange();
+    change_color();
 }
 
-function colorChange() {
+function change_color() {
     var color = ["#FC5C7D", "#6A82FB", "#38ef7d", "#222222", "#b20a2c"];
     var num = Math.floor(Math.random() * color.length);
     h2.style.color = color[num];
