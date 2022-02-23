ccfe6de10bd2d87e7d6384fde699277e479f6bc5
isdiscodead <isdiscodead@korea.ac.kr>
2022-02-23 17:48:53 +0900

    js day 13

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var cnt = 0;
+
 function check_text() {
     var value = document.getElementById('input').value;
     var $message =  document.getElementById('bot_message');
+    var $img = document.getElementById('img');
 
     if ( value == "alert" ) {
-        alert("Warning !");
+        cnt += 1;
+        alert("Warning ! " + cnt);
     } else if ( value == "힘들어") {
         $message.innerHTML = "안 좋은 일이 있나요? 힘들 땐 조금 쉬어도 된다고 생각해요.";
     } else if ( value == "졸려" ) {
@@ -14,6 +18,9 @@ function check_text() {
         $message.innerHTML = "푹 쉬었나요?";
     } else if ( value == "심심해") {
         $message.innerHTML = "할 일은 다 끝난 건가요?";
+    } else if ( value == "배고파") {
+        $message.innerHTML = "밥은 삼시세끼 잘 챙겨먹고 다녀야죠!";
+        $img.src = 'https://i.pinimg.com/originals/ae/85/ba/ae85ba9b17ec2286931931fac65b5fca.gif'
     } else {
         console.log(value);
     }
