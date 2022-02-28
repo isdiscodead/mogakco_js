fe1a83a60b8c7cf3d41da5fafce3011c29c8e73d
isdiscodead <isdiscodead@korea.ac.kr>
2022-02-28 22:37:06 +0900

    solved

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ window.onload = function() {
     // 이름 리스트 ( 임시 ) 
     const names = ["이재명", "심상정", "윤석열"]
     var comparisonItem = [];
+    var exploreQuery = "geo=KR&q=";
 
     for (let i=0 ; i<3 ; i++ ) {
         // 1명 data 생성 후 리스트에 추가
@@ -11,11 +12,15 @@ window.onload = function() {
         data.geo = "KR";
         data.time = "today 1-m";
         comparisonItem.push(data)
+
+        encoded = encodeURI(names[i]);
+        exploreQuery += encoded += ",";
     }
 
-    var jsonData = JSON.stringify(comparisonItem)
+    exploreQuery += "&date=today 1-m,today 1-m";
+    var jsonData = JSON.stringify(comparisonItem);
 
     trends.embed.renderExploreWidget("TIMESERIES", 
-    {"comparisonItem":jsonData, "category":0,"property":""}, 
+    {"comparisonItem":comparisonItem, "category":0,"property":""}, 
     {"exploreQuery":"geo=KR&q=%EC%9D%B4%EC%9E%AC%EB%AA%85,%EC%8B%AC%EC%83%81%EC%A0%95&date=today 1-m,today 1-m","guestPath":"https://trends.google.co.kr:443/trends/embed/"});    
 }
